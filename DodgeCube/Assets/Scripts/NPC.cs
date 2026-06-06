de7945603abe7f5a953125ef192e18a251edbb05
faizyy-bug <faizaan.awan010@gmail.com>
2026-06-06 06:18:28 +0200

    renamed to DodgeCube

---
+++ DodgeCube/Assets/Scripts/NPC.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce;
+    public float sidewaysForce;
+    // for all frames just use -> * Times.deltaTime
+    // WHEN DOING physics and stuff use -> FixedUpdate()
+
+    //public float forwardForce = 2000;
+
+    // void Start()
+    // {
+        
+    // }
+    void FixedUpdate()
+    {
+        // rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
